53e9a36a7f783a0ee00212a39a6cb5443dccd186
John Doe <michael_bevilacqua-linn@comcast.com>
2013-04-10 12:56:04 -0400

    First Cut

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cim-clojure-class
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/cim_clojure_class/core_test.clj
@@ -0,0 +1,7 @@
+(ns cim-clojure-class.core-test
+  (:use clojure.test
+        cim-clojure-class.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
