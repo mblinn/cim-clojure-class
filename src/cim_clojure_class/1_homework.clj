6d0a77546c6cb9d02e220720cab96c1958b40f1c
Michael Bevilacqua-Linn <michael_bevilacqua-linn@comcast.com>
2013-04-26 11:36:35 -0400

    Add homework one.

--- src/cim_clojure_class/1_homework.clj
+++ src/cim_clojure_class/1_homework.clj
@@ -1,2 +1,32 @@
 (ns cim-clojure-class.1-homework)
 
+(def _ nil)
+
+;; Basic Data Structures
+(def a-vector [1 2 3])
+(def a-map {:foo "foo" :bar "bar"})
+(def a-list '("a" "b" "c"))
+
+(= _ a-vector)
+(= _ a-map)
+(= _ a-list)
+
+(= _ (a-vector 0))
+(= _ (a-map :bar))
+
+(= _ (first a-list))
+(= _ (rest a-list))
+
+;; Functions
+(defn add-two-nums [a b] (+ a b))
+
+(= (add-two-nums _ _) 42)
+
+;your prepend-with-hello function goes here.
+(= (prepend-with-hello _) ("Hello, mike"))
+
+;; Read/Eval
+(= _ (read-string "[:foo :bar :baz]"))
+
+(= _ (read-string "(+ 1 1)"))
+(= _ (eval (read-string "(+ 1 1)")))
